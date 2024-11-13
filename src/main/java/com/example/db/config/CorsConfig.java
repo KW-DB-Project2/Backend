f2bcc0928d9c13f3b4dc06f150036460578814fa
gasungfa <gasungfa@gmail.com>
2024-11-13 04:54:55 +0000

    add cicd config

--- src/main/java/com/example/db/config/CorsConfig.java
+++ src/main/java/com/example/db/config/CorsConfig.java
@@ -15,10 +15,10 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");  // 프론트엔드 서버의 주소
+//        config.addAllowedOrigin("http://localhost:5173");  // 프론트엔드 서버의 주소
 //        config.addAllowedOrigin("http://43.200.199.72:5173");  // 프론트엔드 서버의 주소
-//        config.addAllowedOrigin("https://dslquant.site");
-//        config.addAllowedOrigin("https://www.dslquant.site");
+        config.addAllowedOrigin("https://kwangwoon.shop");
+        config.addAllowedOrigin("https://www.kwangwoon.shop");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("authorization");
--- src/main/java/com/example/db/config/WebConfig.java
+++ src/main/java/com/example/db/config/WebConfig.java
@@ -12,9 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:5173") // 허용할 클라이언트 도메인
-                .allowedOrigins("http://43.200.199.72:5173") // 허용할 클라이언트 도메인
-                .allowedOrigins("https://dslquant.site")
-                .allowedOrigins("https://www.dslquant.site")
+//                .allowedOrigins("http://43.200.199.72:5173") // 허용할 클라이언트 도메인
+                .allowedOrigins("https://kwangwoon.shop")
+                .allowedOrigins("https://www.kwangwoon.shop")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 //.allowedHeaders("Content-Type")
                 .allowedHeaders("*")
