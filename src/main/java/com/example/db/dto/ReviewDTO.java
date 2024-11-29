443502102823ce0a1b3ad22cfa7b94895f2d5096
나상연 <reddynor@naver.com>
2024-11-29 22:16:04 +0900

    feat(ReviewDTO.java): Modify Review DTO to have @NoArgsConstructor

--- src/main/java/com/example/db/dto/ReviewDTO.java
+++ src/main/java/com/example/db/dto/ReviewDTO.java
@@ -3,12 +3,14 @@ package com.example.db.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReviewDTO {
     private Long reviewId;
     private Long userId;
